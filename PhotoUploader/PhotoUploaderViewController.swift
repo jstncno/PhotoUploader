c0d2812930d3db141deb010a56a262ec19bef9d3
Justin Cano <jcano001@ucr.edu>
2015-04-29 23:23:39 -0700

    two buttons to read/write to AWS

--- PhotoUploader/PhotoUploaderViewController.swift
+++ PhotoUploader/PhotoUploaderViewController.swift
@@ -29,10 +29,47 @@ class PhotoUploaderViewController: UIViewController, UIImagePickerControllerDele
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject] ) {
         var image = info[UIImagePickerControllerEditedImage] as? UIImage
         if image == nil {
-            image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            image = info[UIImagePickerControllerOriginalImage] as? UIImage
         }
+        
         //process image
-        println("image picked!")
+//        let imageURL = info[UIImagePickerControllerReferenceURL] as! NSURL
+        let uploadRequest = AWSS3TransferManagerUploadRequest()
+        uploadRequest.bucket = "bucket-for-testing"
+        uploadRequest.key = "chelsea-grad.jpg"
+//        uploadRequest.body = imageURL
+//        
+//        let transferManager = AWSS3TransferManager.defaultS3TransferManager()
+//        transferManager.upload(uploadRequest).continueWithExecutor(BFExecutor.mainThreadExecutor(), withBlock: { (task) -> AnyObject! in
+//            if task.error != nil {
+//                println("\(task.error)")
+//                println("Error uploading: \(imageURL)")
+//            } else {
+//                println("Upload completed!")
+//            }
+//            return nil
+//        })
+        
+        
+        
+        if let uploadFilePath = NSBundle.mainBundle().URLForResource("chelsea-grad", withExtension: "jpg") {
+            uploadRequest.body = uploadFilePath
+            println("\(uploadFilePath)")
+            
+            let transferManager = AWSS3TransferManager.defaultS3TransferManager()
+            transferManager.upload(uploadRequest).continueWithExecutor(BFExecutor.mainThreadExecutor(), withBlock: { (task) -> AnyObject! in
+                if task.error != nil {
+                    println("\(task.error)")
+                    println("Error uploading: \(uploadFilePath)")
+                } else {
+                    println("Upload completed!")
+                }
+                return nil
+            })
+        }
+
+//        println("\(imageURL)")
+
         dismissViewControllerAnimated(true, completion: nil)
     }
     
